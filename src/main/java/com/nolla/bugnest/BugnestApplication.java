2ce90880c5dc5736ae27a81b4d4f2c0a843fa4a1
Insecshin <2962479754@qq.com>
2026-08-25 13:08:34 +0800

    chore: initialize BugNest Spring Boot project

---
+++ src/main/java/com/nolla/bugnest/BugnestApplication.java
@@ -0,0 +1,13 @@
+package com.nolla.bugnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugnestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nolla/bugnest/BugnestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nolla.bugnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
