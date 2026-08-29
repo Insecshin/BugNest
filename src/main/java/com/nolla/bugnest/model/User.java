f8428c162b1ae8d3154a8cfd0657e71fc5069675
Insecshin <2962479754@qq.com>
2026-08-29 18:25:55 +0800

    feat: add User model

---
+++ src/main/java/com/nolla/bugnest/model/User.java
@@ -0,0 +1,10 @@
+package com.nolla.bugnest.model;
+
+public class User {
+    private Long id;
+    private String username;
+
+    public User(String username){
+        this.username = username;
+    }
+}
