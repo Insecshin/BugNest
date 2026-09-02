85e53e60d42fb5de0cd46157056c3ce7ba468616
Insecshin <2962479754@qq.com>
2026-09-02 14:41:57 +0800

    feat: add getUser, getAllUsers, deleteUser to UserService

--- src/main/java/com/nolla/bugnest/service/UserService.java
+++ src/main/java/com/nolla/bugnest/service/UserService.java
@@ -3,6 +3,9 @@ package com.nolla.bugnest.service;
 import com.nolla.bugnest.model.User;
 import com.nolla.bugnest.repository.UserRepository;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class UserService {
     private final UserRepository userRepository;
 
@@ -18,5 +21,29 @@ public class UserService {
         User user = new User(username);
 
         return userRepository.save(user);
+    }
+
+    public User getUser(Long id){
+        User user = userRepository.findById(id);
+
+        if(user == null){
+            throw new NoSuchElementException("User not found");
+        }
+
+        return user;
+    }
+
+    public List<User> getALlUsers(){
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(Long id){
+        User user = userRepository.findById(id);
+
+        if(user == null){
+            throw new NoSuchElementException("User not found");
+        }
+
+        userRepository.deleteById(id);
     }
 }
