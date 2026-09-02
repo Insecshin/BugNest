58f31998a2ff07c8bb010c175fb9e33b4c1abfe9
Insecshin <2962479754@qq.com>
2026-09-02 14:49:17 +0800

    test: add tests for getUser, getAllUsers and deleteUser

--- src/test/java/com/nolla/bugnest/service/UserServiceTest.java
+++ src/test/java/com/nolla/bugnest/service/UserServiceTest.java
@@ -4,6 +4,8 @@ import com.nolla.bugnest.model.User;
 import com.nolla.bugnest.repository.MemoryUserRepository;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTest {
@@ -40,5 +42,53 @@ public class UserServiceTest {
         UserService service = new UserService(repository);
 
         assertThrows(IllegalArgumentException.class, () -> service.createUser(null));
+    }
+
+    @Test
+    void shouldGetExistingUser(){
+        MemoryUserRepository repository = new MemoryUserRepository();
+        UserService service = new UserService(repository);
+
+        User created = service.createUser("Noah");
+
+        User found = service.getUser(created.getId());
+
+        assertEquals("Noah", found.getUsername());
+    }
+
+    @Test
+    void shouldThrowWhenUserDoesNotExist(){
+        MemoryUserRepository repository = new MemoryUserRepository();
+        UserService service = new UserService(repository);
+
+        assertThrows(
+                NoSuchElementException.class,
+                () -> service.getUser(999L)
+        );
+    }
+
+    @Test
+    void shouldDeleteExistingUser(){
+        MemoryUserRepository repository = new MemoryUserRepository();
+        UserService service = new UserService(repository);
+
+        User user = service.createUser("Noah");
+
+        service.deleteUser(user.getId());
+
+        assertThrows(
+                NoSuchElementException.class,
+                () -> service.getUser(user.getId())
+        );
+    }
+
+    void shouldThrowWhenDeletingNonExistingUser(){
+        MemoryUserRepository repository = new MemoryUserRepository();
+        UserService service = new UserService(repository);
+
+        assertThrows(
+                NoSuchElementException.class,
+                () -> service.deleteUser(999L)
+        );
     }
 }
